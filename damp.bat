139e64cfad9b0b07929c43ba8b0f636ba0a04b18
Julia <turenko.yuliya@inbox.ru>
2025-04-19 21:02:58 +0800

    nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn

---
+++ damp.bat
@@ -0,0 +1,6 @@
+SET PATH1C="C:\Program Files (x86)\1cv8t\8.3.26.1498\bin\1cv8t.exe"
+SET EXT="C:\Users\turen\OneDrive\Документы\4-course\вкр\test_clone\new-repository-solution\РММ.epf"
+SET SRC="C:\Users\turen\OneDrive\Документы\4-course\вкр\test_clone\new-repository-solution"
+%PATH1C% DESIGNER /DumpExternalDataProcessorOrReportToFiles %SRC% %EXT% IBName "рмм"
+
+  
